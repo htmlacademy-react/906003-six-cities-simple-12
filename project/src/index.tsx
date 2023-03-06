370f50244801632d964fb952aa3cfd221822e3c0
Денис <yegoroff.denis@gmail.com>
2023-03-06 07:02:28 +0300

    Начинаем программировать

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Options = {
+  OffersCount: 6,
+} as const;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App offersCount = {Options.OffersCount} />
   </React.StrictMode>,
 );
